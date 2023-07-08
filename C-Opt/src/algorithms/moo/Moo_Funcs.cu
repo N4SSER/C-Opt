319b8e4872135b449b20bafbb2446c97755e7bd4
Nasser <nasserbrown@estudiantec.cr>
2023-07-07 22:42:06 -0600

    Added parallel evaluators in CUDA

--- C-Opt/src/algorithms/moo/Moo_Funcs.cu
+++ C-Opt/src/algorithms/moo/Moo_Funcs.cu
@@ -3,3 +3,29 @@
 //
 
 #include "Moo_Funcs.cuh"
+
+MOO_func::MOO_func(KernelFunction kernelFunc) : kernel_function(kernelFunc)
+{
+
+}
+
+MOO_func::~MOO_func()
+{
+    cudaFree(d_X);
+    cudaFree(d_Y);
+}
+
+void MOO_func::evaluate(int **X, int *Y, int numElements)
+{
+    cudaMalloc((void**)&d_X, sizeof(int*) * numElements);
+    cudaMemcpy(d_X, X, sizeof(int*) * numElements, cudaMemcpyHostToDevice);
+
+    cudaMalloc((void**)&d_Y, sizeof(int) * numElements);
+
+    int blockSize = 256;
+    int numBlocks = (numElements + blockSize - 1) / blockSize;
+
+    kernel_function<<<numBlocks, blockSize>>>(d_X, d_Y, numElements);
+
+    cudaMemcpy(Y, d_Y, sizeof(int) * numElements, cudaMemcpyDeviceToHost);
+}
--- C-Opt/src/algorithms/moo/Moo_Funcs.cuh
+++ C-Opt/src/algorithms/moo/Moo_Funcs.cuh
@@ -5,54 +5,22 @@
 #ifndef C_OPT_MOO_FUNCS_CUH
 #define C_OPT_MOO_FUNCS_CUH
 
-#include <iostream>
-#include <cuda_runtime.h>
 
-// CUDA kernel function
-__global__ void evaluateFunction(float* values, int n) {
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
+typedef void (*KernelFunction)(int**, int*, int);
 
-    if (index < n) {
-        // Evaluate your function here
-        float result = values[index] * values[index]; // Squaring the input
-
-        // Store the result back to the input array
-        values[index] = result;
-    }
-}
-
-class CudaFunctionEvaluator {
+class MOO_func {
 public:
-    void evaluate(float* values, int n) {
-        float* devValues;
-        cudaMalloc((void**)&devValues, n * sizeof(float));
-        cudaMemcpy(devValues, values, n * sizeof(float), cudaMemcpyHostToDevice);
+    MOO_func(KernelFunction kernelFunc);
+    ~MOO_func();
 
-        int blockSize = 256;
-        int numBlocks = (n + blockSize - 1) / blockSize;
+    void evaluate(int** X, int* Y, int numElements);
 
-        evaluateFunction<<<numBlocks, blockSize>>>(devValues, n);
-
-        cudaMemcpy(values, devValues, n * sizeof(float), cudaMemcpyDeviceToHost);
-        cudaFree(devValues);
-    }
+private:
+    int** d_X;
+    int* d_Y;
+    KernelFunction kernel_function;
 };
 
-int main() {
-    int n = 10;
-    float values[n] = {1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0};
-
-    CudaFunctionEvaluator evaluator;
-    evaluator.evaluate(values, n);
-
-    std::cout << "Results: ";
-    for (int i = 0; i < n; i++) {
-        std::cout << values[i] << " ";
-    }
-    std::cout << std::endl;
-
-    return 0;
-}
 
 
 #endif //C_OPT_MOO_FUNCS_CUH
